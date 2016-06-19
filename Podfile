40c259ba7df6925af7b239c3bdaa502954afce09
steve-dev <steve.dev248@gmail.com>
2016-06-19 15:31:37 -0700

    Mid google sign in feature dev.

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '9.3'
+
+# Google Sign In Pod
+pod 'Google/SignIn'
+
+target 'ios-dev' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ios-dev
+
+  target 'ios-devTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ios-devUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- ios-dev/AppDelegate.swift
+++ ios-dev/AppDelegate.swift
@@ -17,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        // Initialize sign-in
+        var configureError: NSError?
+        GGLContext.sharedInstance().configureWithError(&configureError)
+        assert(configureError == nil, "Error configuring Google services: \(configureError)")
+        
+        GIDSignIn.sharedInstance().delegate = self
+        
         return true
     }
 
